beceb2f029e2273b2f0599fa315ce92f03a4705b
Taiki.Miura <devuser315@gmail.com>
2026-05-02 12:53:59 +0000

    Revert "break handler intentionally to test canary rollback"
    This reverts commit b5c2bc8ded7ae6617f5e196f33397f24403775fb.

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -5,7 +5,6 @@ import os
 VERSION = "v4"
 
 def lambda_handler(event, context):
-    raise Exception("Canary Lambda failed")  # Canary Lambda が失敗することを確認するために例外を発生させる
     return {
         "statusCode": 200,
         "body": json.dumps({
